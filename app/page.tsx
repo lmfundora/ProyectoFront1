1f29865aa21d72b6d8664df0a46bd46ff9ef860a
lmfundora <lmfundora2@gmail.com>
2023-12-13 09:29:21 -0500

    add pictures & react icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,24 @@
 import { NavBar } from "@/components/common/index";
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className="w-full h-full absolute text-white">
+      <Image
+        src='/980238.png'
+        fill
+        alt="Picture of a keyboard"
+      />
       <div className="px-28 z-10 w-full absolute">
         <NavBar />
         <div className="w-1/3 py-28">
-          <p className="text-cyan-600">Welcome to the right place</p>
-          <p className="text-3xl font-bold">Write your own story with the best experience with our highly customizable mechanical keyboards</p>
+          <p className="text-cyan-400 mb-6">Welcome to the right place</p>
+          <p className="text-3xl font-bold">Write your own story with the best experience on our highly customizable mechanical keyboards</p>
           <p className="pt-6 text-md font-serif">Our company has a long history of serving the most demanding clients and always providing the experience that your hands deserve to get the most out of it regardless of your schedule.</p>
         </div>
       </div>
-      <div className="w-1/2 h-full z-0 bg-gradient-to-r from-gray-800 absolute"></div>
+      <div className="w-full h-1/3 z-0 bg-gradient-to-b from-gray-900 absolute"></div>
+      <div className="w-full h-full z-0 bg-gradient-to-r from-gray-950 absolute"></div>
     </div>
   );
 }
--- components/common/NavBar.tsx
+++ components/common/NavBar.tsx
@@ -6,14 +6,14 @@ export default function NavBar() {
       <div className="flex justify-between w-1/2">
         <h2 className="text-2xl font-bold">LogoTowapo</h2>
         <div className="gap-3 flex">
-          <Button variant={'ghost'}>Home</Button>
-          <Button variant={'ghost'}>Products</Button>
-          <Button variant={'ghost'}>Contact</Button>
+          <Button variant={'ghost'} className="rounded-xl">Home</Button>
+          <Button variant={'ghost'} className="rounded-xl">Products</Button>
+          <Button variant={'ghost'} className="rounded-xl">Contact</Button>
         </div>
       </div>
       <div className="flex gap-3">
-        <Button variant={'ghost'}>Login</Button>
-        <Button className="rounded-2xl bg-cyan-600">Sign Up</Button>
+        <Button variant={'ghost'} className="rounded-xl">Login</Button>
+        <Button className="rounded-2xl bg-cyan-400 text-gray-900 hover:text-white hover:bg-blue-600">Sign Up</Button>
       </div>
     </div>
   );
